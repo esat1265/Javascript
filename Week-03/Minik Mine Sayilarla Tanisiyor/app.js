c9d03b96f5dc96f1b1e4e0301731b64291330bd8
Ali baba <seningibimailinben@gmail.com>
2021-11-23 22:38:10 +0100

    assignment has been completed

--- Week-03/Minik Mine Sayilarla Tanisiyor/app.js
+++ Week-03/Minik Mine Sayilarla Tanisiyor/app.js
@@ -1,15 +1,16 @@
-let a =  document.getElementById("ilk sayi").value;
-let b =  document.getElementById("ikinci sayi").value;
+
+ let a = window.prompt('Ilk sayiyi giriniz')
+ let b = window.prompt('Ikinci sayiyi giriniz')
 
 // Aysenin iki sayi girmesini istiyoruz
-// Eger sayilardan biri digerinden buyukse ekrana "Buyuk sayi budur:" yazdiryoruz
 // Eger sayilar esitse ekrana "Bu sayilar esittir." yazdiryoruz
+// Eger sayilardan biri digerinden buyukse ekrana "Buyuk sayi budur:" yazdiryoruz
 
-if(a===b){
+  if(a===b){
     document.getElementById("result").innerHTML = "Bu sayilar esittir.";
     console.log("Bu sayilar esittir.");
-}
-else  if(a>b){
+ }
+ else  if(a>b){
           document.getElementById("result").innerHTML = ("Buyuk sayi:" + a);
           console.log("Buyuk sayi:" + a);
         } 
@@ -17,3 +18,4 @@ else  if(a>b){
           document.getElementById("result").innerHTML = ("Buyuk sayi:"+ b);
           console.log("Buyuk sayi:" + b);
         }
+ 
